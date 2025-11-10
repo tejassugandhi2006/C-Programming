1a4f2452cc4ce754dbfd849a02cdc838d3aee67f
TEJAS P SUGANDHI <tejassugandhi02@gmail.com>
2025-11-10 18:14:02 +0530

    Delete Programs directory

--- Programs/SimpleIntrest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float p, r, t, si;
-    printf("Enter principal, rate, and time: ");
-    scanf("%f %f %f", &p, &r, &t);
-    si = (p * r * t) / 100;
-    printf("Simple interest = %.2f", si);
-    return 0;
-}
